fd33115dba1c2edf646b82c4ff6a3ae65fcbc7b5
Eric Leonardo Lim <ericl619@gmail.com>
2018-03-25 11:11:41 +0800

    changed to IPtrace node aggregate

--- test.cpp
+++ test.cpp
@@ -23,6 +23,7 @@ using namespace std;
 #define NUM_OF_LINES 78508963
 */
 
+/*
 #define GRAPH_STREAM_FILE "graph_shuffled.txt"
 //#define DATA_SAMPLE_FILE "graph_freq_comp1_reservoir5.txt"
 #define DATA_SAMPLE_FILE "graph_freq_comp1_reservoir5_SORTED_0.171396_0_.txt"
@@ -30,15 +31,16 @@ using namespace std;
 #define P 56175521
 #define total_freq 9812800185LL
 #define NUM_OF_LINES 1372146644
+*/
 
-/*
+
 #define GRAPH_STREAM_FILE "ip_graph_refined"
 #define DATA_SAMPLE_FILE "ip_graph_refined_reservoir5.txt"
 #define QUERY_FILE "ip_graph_refined_reservoir2.txt"
 #define P 4213103
 #define total_freq 436186619LL
 #define NUM_OF_LINES 12714850
-*/
+
 
 #define w0 400
 #define C 0.1
@@ -421,20 +423,20 @@ int main()
 			//{
 			//	ffout << app.query(u, v) << " " << freq << " " << u << " " << v << '\n';
 			//}
-			if (freq >= total_freq / HH_CONSTANT)
-				heavy1.insert({u, v});
+			//if (freq >= total_freq / HH_CONSTANT)
+			//	heavy1.insert({u, v});
 
 			//if(aggFreq.count(u)) aggFreq[u] += freq;
 			//else aggFreq[u] = freq;
 
-            //if(aggFreq.count(v)) aggFreq[v] += freq;
-            //else aggFreq[v] = freq;
+            if(aggFreq.count(v)) aggFreq[v] += freq;
+            else aggFreq[v] = freq;
 		}
 
-		//evalNodeAgg2(app,control);
-		//evaluate1x(app,control);
-		heavyHitter(HH_CONSTANT,heavy1,app);
-		heavyHitterControl(HH_CONSTANT,heavy1,control);
+		evalNodeAgg2(app,control);
+		evaluate1x(app,control);
+		//heavyHitter(HH_CONSTANT,heavy1,app);
+		//heavyHitterControl(HH_CONSTANT,heavy1,control);
 	}
 
 
